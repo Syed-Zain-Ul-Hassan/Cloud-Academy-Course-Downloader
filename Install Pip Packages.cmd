2533f9dfaa4d12d3f91eebbb77021635e7be80ab
Zain Ul Hassan <37849034+Syed-Zain-Ul-Hassan@users.noreply.github.com>
2021-03-28 10:05:27 +0000

    Install Pip Packages
    This CMD file will install your all required pip packages.

---
+++ Install Pip Packages.cmd
@@ -0,0 +1,13 @@
+ECHO =========================
+INSTALLING PIP PACKAGES
+ECHO =========================
+pip install beautifulsoup4 
+pip install bs4 
+pip install lxml 
+pip install selenium 
+pip install times2 
+pip install tkintertable 
+pip install urllib3 
+pip install wget
+ECHO =========================
+PAUSE
